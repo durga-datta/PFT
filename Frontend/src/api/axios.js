b0bcd0d6e0842385fc34c61caec378b78eddc38a
Durgadatta Sahoo <166171630+durga-datta@users.noreply.github.com>
2026-01-12 11:53:58 +0530

    Update API base URL to production endpoint

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pft-backend-0di1.onrender.com/",
 });
 
 api.interceptors.request.use(
