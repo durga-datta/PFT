83cf576b1df43f4eea979b0fe5ac658a5fb8b76e
Durgadatta Sahoo <166171630+durga-datta@users.noreply.github.com>
2026-01-12 11:56:22 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pft-backend-0di1.onrender.com/",
+  baseURL: "https://pft-backend-0di1.onrender.com/api",
 });
 
 api.interceptors.request.use(
