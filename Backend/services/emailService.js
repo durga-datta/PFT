07d14d06b1760be58e89e863977981e80ceedc45
durga-datta <durgadatta573@gmail.com>
2026-01-18 11:58:59 +0530

    test-6

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -1,17 +1,16 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  logger:true,
+  debug: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 60000,
-  greetingTimeout: 30000,
-  socketTimeout: 60000
+  }
 });
 
 export const sendEmail = ({ to, subject, text, html }) => {
